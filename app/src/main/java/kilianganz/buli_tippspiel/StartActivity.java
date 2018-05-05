708cde56b5014c6e05e00c25cd8e7706a98a16fc
Kilian Ganz <kiliganz@web.de>
2018-05-05 18:45:04 +0200

    First Development of Firebase E-Mail Auth 0.1

---
+++ app/src/main/java/kilianganz/buli_tippspiel/StartActivity.java
@@ -0,0 +1,16 @@
+package kilianganz.buli_tippspiel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        Log.d("Testausgabe", "funktioniert");
+    }
+}

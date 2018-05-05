9b6c91e7d8b02765b272784d1b1cc021eb510a85
Kilian Ganz <kiliganz@web.de>
2018-05-05 14:08:17 +0200

    Init 1.1

---
+++ app/src/main/java/kilianganz/buli_tippspiel/MainActivity.java
@@ -0,0 +1,13 @@
+package kilianganz.buli_tippspiel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

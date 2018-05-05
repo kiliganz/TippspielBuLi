51a899aa476c4e995694c49222409b7fb12166e6
Lukas Trockel <trockellukas@gmail.com>
2018-05-05 23:51:49 +0200

    Klasse databaseHandler mit Methoden getTip und getRes hinzugefügt. Debugging weitestgehend aus calculateResult raus.

--- app/src/main/java/kilianganz/buli_tippspiel/calculateResult.java
+++ app/src/main/java/kilianganz/buli_tippspiel/calculateResult.java
@@ -5,94 +5,89 @@ import android.util.Log;
 import java.util.ArrayList;
 
 public class calculateResult {
-    private int drawPts = 5;
-    private int drawTendPts = 2;
-    private int defDiffPts = 3;
-    private int defTendPts = 2;
-    private int defPts = 5;
-    ArrayList<Integer> tip = new ArrayList<Integer>();
+    private int drawPts = 50;
+    private int drawTendPts = 25;
+    private int defDiffPts = 30;
+    private int defTendPts = 20;
+    private int defPts = 70;
 
-    ArrayList<Integer> res = new ArrayList<Integer>();
+    //DatabaseHandler-Klasse muss implementiert werden!
+    databaseHandler dataHandler;
 
-    private void getLogVals() {
-        tip.add(0);
-        tip.add(1);
-        res.add(1);
-        res.add(0);
-    }
-
-    //database xy;
-    /*
     private ArrayList<Integer> getTip(String game) {
-        ArrayList<Integer> out = xy.getTip(game);
+        ArrayList<Integer> out = dataHandler.getTip(game);
         return out;
     }
 
     private ArrayList<Integer> getRes(String game) {
-        ArrayList<Integer> out = xy.getRes(game);
+        ArrayList<Integer> out = dataHandler.getRes(game);
         return out;
     }
-    */
+
 
     public int calculatePoints(ArrayList<String> matches) {
+
+
         int sumPts = 0;
 
 
         for (String match : matches) {
-            getLogVals();
 
             boolean draw = false;           //Switch for matches with draw
 
-            ArrayList<Integer> tempTip = tip;
-            //ArrayList<Integer> tempTip = getTip(match);
+            ArrayList<Integer> tempTip = getTip(match);
             String tipp = "";
-            ArrayList<Integer> tempRes = res;
-            //ArrayList<Integer> tempRes = getRes(match);
+            //Was wenn noch keine Ergebnisse vorhanden?
+            ArrayList<Integer> tempRes = getRes(match);
             String result = "";
+            if (tempTip != null && tempRes != null) {
+                int tipDif = tempTip.get(0) - tempTip.get(1);
+                int resDif = tempRes.get(0) - tempRes.get(1);
 
-            int tipDif = tempTip.get(0) - tempTip.get(1);
-            int resDif = tempRes.get(0) - tempRes.get(1);
-
-            if (tipDif == 0 && resDif == 0) {
-                draw = true;
-            } else {
-                draw = false;
-            }
-
-            for (int i = 0; i < tempTip.size(); i++) {
-                tipp += tempTip.get(i).toString();
-                if (i == 0) {
-                    tipp += " - ";
-                }
-            }
-            for (int i = 0; i < tempRes.size(); i++) {
-                result += tempRes.get(i).toString();
-                if (i == 0) {
-                    result += " - ";
-                }
-            }
-            Log.d("Tipp: ", tipp );
-            Log.d("Ergebnis: ", result );
-
-            if (draw) {
-                if (tempTip.get(0) == tempTip.get(0)) {
-                    sumPts += drawPts;
+                if (tipDif == 0 && resDif == 0) {
+                    draw = true;
                 } else {
-                    sumPts += drawTendPts;
+                    draw = false;
                 }
+
+                for (int i = 0; i < tempTip.size(); i++) {
+                    tipp += tempTip.get(i).toString();
+                    if (i == 0) {
+                        tipp += " - ";
+                    }
+                }
+                for (int i = 0; i < tempRes.size(); i++) {
+                    result += tempRes.get(i).toString();
+                    if (i == 0) {
+                        result += " - ";
+                    }
+                }
+                Log.d("Tipp: ", tipp);
+                Log.d("Ergebnis: ", result);
+
+                if (draw) {
+                    if (tempTip.get(0) == tempRes.get(0)) {
+                        sumPts += drawPts;
+                    } else {
+                        sumPts += drawTendPts;
+                    }
+                } else {
+                    if ((tipDif == resDif) && tempTip.get(0) == tempRes.get(0)) {
+                        sumPts += defPts;
+                    } else if (tipDif == resDif && tempTip.get(0) != tempRes.get(0)) {
+                        sumPts += defDiffPts;
+                    } else if (Integer.signum(tipDif) == Integer.signum(resDif)) {
+                        sumPts += defTendPts;
+                    }
+                }
+
+
+
             } else {
-                if ((tipDif == resDif) && tempTip.get(0) == tempRes.get(0)) {
-                    sumPts += defPts;
-                } else if (tipDif == resDif && tempTip.get(0) != tempRes.get(0)) {
-                    sumPts += defDiffPts;
-                } else if (Integer.signum(tipDif) == Integer.signum(resDif)) {
-                    sumPts += defTendPts;
-                }
+                Log.d("Achtung!", "Begegnung hat keinen Tipp oder noch kein Ergebnis!");
             }
-
-
         }
-        Log.d("Summe: ","i = " + sumPts );
+        Log.d("Summe: ", "i = " + sumPts);
         return sumPts;
     }
 }
---
+++ app/src/main/java/kilianganz/buli_tippspiel/databaseHandler.java
@@ -0,0 +1,22 @@
+package kilianganz.buli_tippspiel;
+
+import java.util.ArrayList;
+
+public class databaseHandler {
+
+
+
+    public ArrayList<Integer> getTip(String match) {
+        ArrayList<Integer> out = new ArrayList<Integer>();
+
+
+
+        return out;
+    }
+
+    public ArrayList<Integer> getRes(String match) {
+        ArrayList<Integer> out = new ArrayList<Integer>();
+        return out;
+
+    }
+}
